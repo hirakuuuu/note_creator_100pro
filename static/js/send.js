6be16f7fedb0d3ca7bd82441dc71692b7425a147
hirakuuuu <m.hiraku1129@gmail.com>
2022-08-31 16:35:08 +0900

    [add] textareaに出力結果を表示する機能

--- static/js/send.js
+++ static/js/send.js
@@ -12,9 +12,20 @@ $("#submitButton").click(function () {
     .done(function (received_data) {
       var dict = JSON.parse(received_data);
       var answer = dict["answer"];
+      var text = "";
+      var colsize = 0;
+      var rowsize = answer.length * 3;
       for (const elem of answer) {
-        $("#predict").append($("<p>").append(elem));
+        // $("#predict").append($("<p>").append(elem));
+        text += elem + "\n" + "\n";
+        colsize = Math.max(colsize, elem.length);
       }
+      // textareaにテキストを入れる
+      var textform = document.getElementById("textform");
+      textform.style.display = "block";
+      textform.value = text;
+      textform.cols = (colsize * 2).toString();
+      textform.rows = rowsize.toString();
       console.log("成功");
     })
     .fail(function () {
